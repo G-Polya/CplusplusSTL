0c15d276b44baceba83ae51f98da6dd0d2df0b11
김희수 <wlskaka4@gmail.com>
2017-04-05 17:23:02 +0900

    sad

--- Cpp/Cpp/Header.h
+++ Cpp/Cpp/Header.h
@@ -1,22 +1,38 @@
 #pragma once
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+using std::ostream;
+using std::istream;
 
-class StringBad
+class String
 {
 private:
 	char *str;
 	int len;
 	static int num_strings;
+	static const int CINLIM = 80;
 
 public:
-	StringBad(const char *s);
-	StringBad();
-	~StringBad();
-	StringBad(const StringBad& st);
-	
-	StringBad& operator=(const StringBad& st);
+	//생성자와 기타 메서드
+	String(const char *s);
+	String();
+	String(const String& st);
+	~String();
+	int length() const { return len; }
 
-	friend std::ostream& operator<<(std::ostream& os, const StringBad& st);
+	//오버로딩 연산자 메서드
+	String& operator=(const String& st);
+	String& operator=(const char *s);
+	char& operator[](int i);
+	const char& operator[](int i)const;
 
+	//오버로딩 연산자 프렌드
+	friend bool operator<(const String& st1, const String& st2);
+	friend bool operator>(const String& st1, const String& st2);
+	friend bool operator==(const String& st1, const String& st2);
+	friend ostream& operator<<(ostream& os, const String& st);
+	friend istream& operator>>(istream& is, const String& st);
+
+	//static 함수
+	static int HowMany();
 };
--- Cpp/Cpp/Source.cpp
+++ Cpp/Cpp/Source.cpp
@@ -2,53 +2,51 @@
 #include <cstring>
 #include "Header.h"
 
+using std::cin;
 using std::cout;
 
+//static 클래스 멤버를 초기화한다
+int String::num_strings = 0;
 
-int StringBad::num_strings = 0;
+//static 메서드
+int String::HowMany()
+{
+	return num_strings;
+}
 
-
-//C스타일 문자열로부터 StringBad를 생성
-StringBad::StringBad(const char *s)
+//클래스 메서드
+String::String(const char *s)
 {
 	len = std::strlen(s);
 	str = new char[len + 1];
 	std::strcpy(str, s);
 	num_strings++;
-	cout << num_strings << ": \"" << str << "\" 객체 생성\n";
 }
 
-//default constructor
-StringBad::StringBad()
+String::String() //디폴트 생성자
 {
 	len = 4;
-	str = new char[4];
-	std::strcpy(str, "C++");
+	str = new char[1];
+	str[0] = '\0';
 	num_strings++;
-	cout << num_strings << ": \"" << str << "\" 디폴트 객체 생성\n";
 }
 
-//꼭 필요한 파괴자
-StringBad::~StringBad()
-{
-	cout << "\"" << str << "\" 객체파괴, ";
-	--num_strings;
-	cout << "남은 객체 수: " << num_strings << "\n";
-	delete[] str;
-}
-
-//복사생성자
-StringBad::StringBad(const StringBad& st)
+String::String(const String& st)	//복사 생성자
 {
 	num_strings++;
 	len = st.len;
 	str = new char[len + 1];
 	std::strcpy(str, st.str);
-	cout << num_strings << ": \"" << str << "\"객체 생성\n";
 }
 
-//대입연산자
-StringBad& StringBad::operator=(const StringBad& st)
+String::~String()
+{
+	--num_strings;
+	delete[] str;
+}
+
+//오버로드 연산자 메서드
+String& String::operator=(const String& st)
 {
 	if (this == &st)
 		return *this;
@@ -59,8 +57,55 @@ StringBad& StringBad::operator=(const StringBad& st)
 	return *this;
 }
 
-std::ostream& operator<<(std::ostream& os, const StringBad& st)
+String& String::operator=(const char *s)
+{
+	delete[] str;
+	len = std::strlen(s);
+	str = new char[len + 1];
+	std::strcpy(str, s);
+	return *this;
+}
+
+char& String::operator[](int i)
+{
+	return str[i];
+}
+
+const char& String::operator[](int i) const
+{
+	return str[i];
+}
+
+//오버로딩 연산자 프렌드
+bool operator<(const String& st1, const String& st2)
+{
+	return (std::strcmp(st1.str, st2.str) < 0);
+}
+
+bool operator>(const String& st1, const String& st2)
+{
+	//return (std::strcmp(st1.str, st2.str) > 0);
+	return st1.str > st2.str;
+}
+
+bool operator==(const String& st1, const String& st2)
+{
+	return (std::strcmp(st1.str, st2.str) == 0);
+}
+
+ostream& operator<<(ostream& os, const String& st)
 {
 	os << st.str;
 	return os;
+}
+
+istream& operator>>(istream& is, String& st)
+{
+	char temp[String::CINLIM];
+	is.get(temp, String::CINLIM);
+	if (is)
+		st = temp;
+	while (is && is.get() != '\n')
+		continue;
+	return is;
 }
