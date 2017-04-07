02c97598a7c5d3e35f000f8558371a1662879ebb
김희수 <wlskaka4@gmail.com>
2017-04-07 16:37:01 +0900

    zx

--- Cpp/Cpp/main.cpp
+++ Cpp/Cpp/main.cpp
@@ -1,46 +1,55 @@
-
 #include <iostream>
-using std::cout;
 #include "Header.h"
 
-void callme1(StringBad&);
-void callme2(StringBad);
+const int ArSize = 10;
+const int MaxLen = 81;
 
 int main()
 {
+	using std::cout;
+	using std::cin;
 	using std::endl;
+	String name;
+	cout << "안녕하십니까? 성함이 어떻게 되십니까?\n>> ";
+	cin >> name;
+
+	cout << name << " 씨! 간단한 영어속담 " << ArSize
+		<< "개만 입력해주십시오(끝내려면 Enter):\n";
+	String sayings[ArSize];									//객체들의 배열
+	char temp[MaxLen];
 	
-	StringBad headline1("Celery Stalks at Midnight");
-	StringBad headline2("Lettuce Prey");
-	StringBad sports("Spinach Leaves Bowl for Dollars");
-	
-	cout << "headline1: " << headline1 << endl;
-	cout << "headline2: " << headline2 << endl;
-	cout << "sports: " << sports << endl;
-	callme1(headline1);
-	cout << "headline1: " << headline1 << endl;
-	callme2(headline2);
-	cout << "headline2: " << headline2 << endl;
-	cout << "하나의 객체를 다른 객체로 초기화: \n";
-	StringBad sailor = sports;
-	cout << "sailor: " << sailor << endl;
-	cout << "하나의 객체를 다른 객체에 대입: \n";
-	StringBad knot;
-	knot = headline1;
-	cout << "knot: " << knot << endl;
-	cout << "main()의 끝\n";
+	int i;
+	for ( i = 0; i < ArSize; i++)
+	{
+		cout << i + 1 << ": ";
+		cin.get(temp, MaxLen);
+		while (cin && cin.get() != '\n')
+			continue;
+		if (!cin || temp[0] == '\0')						
+			break;											//입력이 실패했거나 빈 줄입력이면 i를 증가시키지 않는다
+		else
+			sayings[i] = temp;								//오버로딩 대입 연산자를 사용한다.
+	}
+
+	int total = i;											//읽어 들인 총 줄 수
+
+	cout << "(다음과 같은 속담들을 입력하셨습니다.)\n";
+	for (i = 0; i < total; i++)
+		cout << sayings[i][0] << ": " << sayings[i] << endl;
+
+	int shortest = 0;
+	int first = 0;
+	for (i = 1; i < total; i++)
+	{
+		if (sayings[i].length() < sayings[shortest].length())
+			shortest = i;
+		if (sayings[i] < sayings[first])
+			first = i;
+	}
+
+	cout << "가장 짧은 속담:\n" << sayings[shortest] << endl;
+	cout << "사전순으로 가장 앞에 나오는 속담:\n" << sayings[first] << endl;
+	cout << "이 프로그램은 " << String::HowMany() << "개의 String객체를 사용하였습니다.이상!\n";
 
 	return 0;
-}
-
-void callme1(StringBad& rsb)
-{
-	cout << "참조로 전달된 StringBad: \n";
-	cout << "     \"" << rsb << "\"\n";
-}
-
-void callme2(StringBad sb)
-{
-	cout << "값으로 전달된 StringBad:\n";
-	cout << "      \"" << sb << "\"\n";
 }
