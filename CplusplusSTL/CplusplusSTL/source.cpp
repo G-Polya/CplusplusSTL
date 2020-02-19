fa02bb7db004f727f04f36aaeae6c5679ed79d50
G_Polya <wlskaka4@gmail.com>
2020-02-19 11:50:55 +0900

    test

--- CplusplusSTL/CplusplusSTL/source.cpp
+++ CplusplusSTL/CplusplusSTL/source.cpp
@@ -6,31 +6,50 @@
 
 using namespace std;
 
+class Box
+{
+private:
+	double length, width, height;
+public:
+	Box(double length, double width, double height)
+		: length(length), width(width), height(height)
+	{}
+
+	double getLength() const
+	{
+		return this->length;
+	}
+
+	double getWidth() const
+	{
+		return this->width;
+	}
+
+	double getHeight() const
+	{
+		return this->height;
+	}
+
+};
+
+class Volume
+{
+public:
+	double operator()(double x, double y, double z)
+	{
+		return x * y * z;
+	}
+
+	double operator()(const Box& box)
+	{
+		return box.getLength() * box.getWidth() * box.getHeight();
+	}
+};
 
 int main()
 {
-	string name = "Jack";
-	auto pname = make_unique<string>(name);
-
-	cout << "string객체 name의 주소: " << &name << endl;
-	cout << "string객체 name의 내용: " << name << endl;
-	cout << "유니크 포인터 객체 pname의 주소: " << &pname << endl;
-	cout << "유니크 포인터 객체 pname값: " << pname << endl;
-	cout << "유니크 포인터 객체 pname값이 가리키는 내용: " << *pname << endl << endl;
-
-	int num = 10;
-	auto pnum = make_unique<int>(num);
-	cout << "int형 num의 주소: " << &num << endl;
-	cout << "int형 num의 값: " << num << endl;
-	cout << "유니크 포인터 객체 pnum의 주소: " << &pnum << endl;
-	cout << "유니크 포인터 객체 pnum의 값: " << pnum << endl;
-	cout << "유니크 포인터 객체 pnum의 값이 가리키는 내용: " << *pnum << endl << endl;
-
-	int* num2 = (int*)malloc(sizeof(int));
-	*num2 = 20;
-	cout << "malloc int형 num2의 주소: " << &num2 << endl;
-	int* num3 = new int(30);
-	cout << "new int형 num3의 주소:" << &num3 << endl;
-
+	Volume volume;
+	Box box( 1.0, 2.0, 3.0 );
+	cout << "상자의 두피" << volume(box) << endl;
 	return 0;
 }
