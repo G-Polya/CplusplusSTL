fa240f20d4e215a6e02fcbbf541429697810807c
Acbik <Greedy@msi>
2020-02-21 11:00:28 +0900

    temp

--- CplusplusSTL/CplusplusSTL/source.cpp
+++ CplusplusSTL/CplusplusSTL/source.cpp
@@ -9,15 +9,23 @@ using namespace std;
 
 int main()
 {
-	vector<string> words;
-	words.push_back(string("facetious"));
-	words.emplace_back("abstemious");
-
-	string str = "alleged";
-	words.emplace_back(str, 2, 3);
-
+	vector<string> words = { "one","three","eight" };
 	copy(begin(words), end(words), ostream_iterator<string>(cout, " "));
+	cout << endl;
+	auto iter = words.insert(++begin(words), "two");
+	copy(begin(words), end(words), ostream_iterator<string>(cout, " "));
+	cout << endl;
+	string more[] = { "five", "six", "seven" };
+	iter = words.insert(--end(words), begin(more), end(more));
+	copy(begin(words), end(words), ostream_iterator<string>(cout, " "));
+	cout << endl;
+	iter = words.insert(end(words), "ten");
+	copy(begin(words), end(words), ostream_iterator<string>(cout, " "));
+	cout << endl;
 
+	iter = words.insert(end(words), { string("twelve"), string("thirteen") });
+	copy(begin(words), end(words), ostream_iterator<string>(cout, " "));
+	cout << endl;
 
 	return 0;
 }
