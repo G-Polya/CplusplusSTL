79555dc8cf52f6becd7b2bc2d4b25d40e0557077
G_Polya <wlskaka4@gmail.com>
2020-03-07 22:18:20 +0900

    2.5 list 컨테이너

--- CplusplusSTL/CplusplusSTL/source.cpp
+++ CplusplusSTL/CplusplusSTL/source.cpp
@@ -1,23 +1,41 @@
 #include <iostream>
-#include <iomanip>
-#include <array>
-#include <vector>
-#include <algorithm>
-#include <iterator>
-#include <deque>
+#include <list>
+#include <string>
+#include <functional>
+
 using namespace std;
 
+template<typename Iter>
+void list_elements(Iter begin, Iter end)
+{
+	while (begin != end)
+	{
+		cout << *begin++ << endl;
+	}
+}
 
 int main()
 {
-	deque<string> names;
-	cout << "Enter first names separated by spaces. Enter Ctrl+Z on a new line to end: \n";
-	copy(istream_iterator<string>(cin), istream_iterator<string>(), front_inserter(names));
+	list<string> proverbs;
 
-	cout << "\nIn reverse order, the names you entered are:\n";
-	copy(begin(names), end(names), ostream_iterator<string>(cout, " "));
+	cout << "Enter a few proverbs and enter an empty line to end: " << endl;
+	string proverb;
+	while (getline(cin, proverb, '\n'), !proverb.empty())
+		proverbs.push_front(proverb);
+	cout << "Go on, just one more:" << endl;
+	getline(cin, proverb, '\n');
+	proverbs.emplace_back(proverb);
 
-	cout << endl;
+	cout << "\nThe elements in the list in reverse ordera are : " << endl;
+	list_elements(rbegin(proverbs), rend(proverbs));
+
+	proverbs.sort();
+	cout << "\nYour proverbs in ascending sequence are: " << endl;
+	list_elements(begin(proverbs), end(proverbs));
+
+	proverbs.sort(greater<>());
+	cout << "\nYour proverbs in descending sequence: " << endl;
+	list_elements(begin(proverbs), end(proverbs));
 
 	return 0;
 }
